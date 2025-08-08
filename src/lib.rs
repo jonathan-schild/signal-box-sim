45d8c09b361bb11201d1fc7322d76c0b8bf8e07e
Jonathan "Nath" Schild <nath@j-schild.de>
2025-08-09 00:42:18 +0200

    Start restructure

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,10 @@ use std::io;
 
 use thiserror::Error;
 
-pub mod model;
-pub mod parser;
-pub mod svg_render;
-
 #[derive(Debug, Error)]
 pub enum Error {
     #[error(transparent)]
     Io(#[from] io::Error),
-    #[error("Unexpected symbol: {0}; expected: {1}")]
-    UnexpectedSymbol(char, String),
-    #[error("Expected: {0}")]
-    ExpectedToken(String),
-    #[error("Unexpected EOF")]
-    UnexpectedEOF,
-    #[error("Invalid identifier: {0}")]
-    InvalidIdentifier(String),
     #[error("Unknown Error")]
     Unknown,
 }
